04a10dac646ed62bdf620387c13a1124a2fa3a9b
AriannFernando <A01703556@itesm.mx>
2024-05-16 00:46:21 -0600

    data split images implemented

--- DataSplit.py
+++ DataSplit.py
@@ -3,7 +3,7 @@ import shutil
 import random
 
 # Definir las rutas de las carpetas de origen y destino
-origin_path = 'asl_alphabet_train'
+origin_path = 'dog_emotion'
 destination_path_train = 'train'
 destination_path_test = 'test'
 destination_path_validation = 'validation'
@@ -16,14 +16,14 @@ if not os.path.exists(destination_path_test):
 if not os.path.exists(destination_path_validation):
     os.makedirs(destination_path_validation)
 
-# Obtener la lista de carpetas de letras
-letters_directory = os.listdir(origin_path)
+# Obtener la lista de carpetas de dentro del la carpeta principal (las clases)
+classes_directory = os.listdir(origin_path)
 
-# Iterar sobre cada carpeta de letra
-for letter_directory in letters_directory:
-    # Obtener la lista de imágenes en la carpeta de letra
-    letter_directory_path = os.path.join(origin_path, letter_directory)
-    images = os.listdir(letter_directory_path)
+# Iterar sobre cada carpeta (clase)
+for class_directory in classes_directory:
+    # Obtener la lista de imágenes en la clase
+    class_directory_path = os.path.join(origin_path, class_directory)
+    images = os.listdir(class_directory_path)
     
     # Mezclar aleatoriamente las imágenes
     random.shuffle(images)
@@ -34,16 +34,16 @@ for letter_directory in letters_directory:
     num_test = int(0.1 * total_images)
     num_validation = total_images - num_train - num_test
     
-    # Crear las carpetas de destino dentro de la carpeta de letra si no existen
-    letter_train_path = os.path.join(destination_path_train, letter_directory)
-    letter_test_path = os.path.join(destination_path_test, letter_directory)
-    letter_validation_path = os.path.join(destination_path_validation, letter_directory)
-    if not os.path.exists(letter_train_path):
-        os.makedirs(letter_train_path)
-    if not os.path.exists(letter_test_path):
-        os.makedirs(letter_test_path)
-    if not os.path.exists(letter_validation_path):
-        os.makedirs(letter_validation_path)
+    # Crear las carpetas de destino dentro de la carpeta clase si no existen
+    class_train_path = os.path.join(destination_path_train, class_directory)
+    class_test_path = os.path.join(destination_path_test, class_directory)
+    class_validation_path = os.path.join(destination_path_validation, class_directory)
+    if not os.path.exists(class_train_path):
+        os.makedirs(class_train_path)
+    if not os.path.exists(class_test_path):
+        os.makedirs(class_test_path)
+    if not os.path.exists(class_validation_path):
+        os.makedirs(class_validation_path)
     
     # Dividir las imágenes en conjuntos (train, test, validation)
     images_train = images[:num_train]
@@ -52,8 +52,8 @@ for letter_directory in letters_directory:
     
     # Mover las imágenes a las carpetas de destino copiandolas
     for image in images_train:
-        shutil.copy(os.path.join(letter_directory_path, image), os.path.join(letter_train_path, image))
+        shutil.copy(os.path.join(class_directory_path, image), os.path.join(class_train_path, image))
     for image in images_test:
-        shutil.copy(os.path.join(letter_directory_path, image), os.path.join(letter_test_path, image))
+        shutil.copy(os.path.join(class_directory_path, image), os.path.join(class_test_path, image))
     for image in images_validation:
-        shutil.copy(os.path.join(letter_directory_path, image), os.path.join(letter_validation_path, image))
+        shutil.copy(os.path.join(class_directory_path, image), os.path.join(class_validation_path, image))
